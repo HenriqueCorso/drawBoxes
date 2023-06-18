9ee16a240a07f0a57abb31ab9e2d027d4cad6438
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-18 14:38:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const main = () => {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  let isDrawing = false;
+  let startX, startY;
+
+  canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    startX = event.offsetX;
+    startY = event.offsetY;
+  });
+
+  canvas.addEventListener('mousemove', (event) => {
+    if (!isDrawing) return;
+    const currentX = event.offsetX;
+    const currentY = event.offsetY;
+
+    drawBox(startX, startY, currentX, currentY);
+  });
+
+  canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+  });
+
+  const drawBox = (startX, startY, endX, endY) => {
+    const width = endX - startX;
+    const height = endY - startY;
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.rect(startX, startY, width, height);
+    context.fill();
+  };
+}
+main();
