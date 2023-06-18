6f7638563cad11a35809c19c123528f9f5b64cfd
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-18 19:21:52 -0300

    Update script.js
    add image to canvas

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const main = () => {
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
   const colorPicker = document.getElementById('colorPicker');
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  const fileInput = document.getElementById('fileInput');
 
   let isDrawing = false;
   let startX, startY;
+
+  // default canvas
+  const setCanvasDimensions = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
+  // new canvas
+  const drawImageOnCanvas = (image) => {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  };
 
   canvas.addEventListener('mousedown', (event) => {
     isDrawing = true;
@@ -27,20 +37,35 @@ const main = () => {
     isDrawing = false;
   });
 
+  // upload image
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const image = new Image();
+      image.onload = () => {
+        drawImageOnCanvas(image);
+      };
+      image.src = event.target.result;
+    };
+    // read image
+    reader.readAsDataURL(file);
+  });
+
   const drawBox = (startX, startY, endX, endY) => {
     const width = endX - startX;
     const height = endY - startY;
-
     const selectedColor = colorPicker.value;
 
     context.strokeStyle = 'black';
     context.lineWidth = 2;
-
     context.fillStyle = selectedColor;
-
     context.beginPath();
     context.rect(startX, startY, width, height);
     context.fill();
   };
-}
+  setCanvasDimensions();
+};
+
 main();
