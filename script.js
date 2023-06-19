a3b49488a575a13afd1b321816af7701caa6e788
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-18 21:52:27 -0300

    Update script.js
    add pencil and eraser

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ const main = () => {
   const context = canvas.getContext('2d');
   const colorPicker = document.getElementById('colorPicker');
   const fileInput = document.getElementById('fileInput');
+  const pencilButton = document.getElementById('pencilButton');
+  const eraserButton = document.getElementById('eraserButton');
+  const boxButton = document.getElementById('boxButton');
 
   let isDrawing = false;
   let startX, startY;
+  let selectedTool = 'box';
 
-  // default canvas
   const setCanvasDimensions = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   };
-  // new canvas
+
   const drawImageOnCanvas = (image) => {
     canvas.width = image.width;
     canvas.height = image.height;
     context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  };
+
+  const setTool = (tool) => {
+    selectedTool = tool;
+  };
+
+  const draw = (startX, startY, endX, endY) => {
+    const width = endX - startX;
+    const height = endY - startY;
+    const selectedColor = colorPicker.value;
+
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.fillStyle = selectedColor;
+
+    if (selectedTool === 'pencil') {
+      context.beginPath();
+      context.moveTo(startX, startY);
+      context.lineTo(endX, endY);
+      context.stroke();
+    } else if (selectedTool === 'eraser') {
+      context.clearRect(startX, startY, width, height);
+    } else if (selectedTool === 'box') {
+      context.beginPath();
+      context.rect(startX, startY, width, height);
+      context.fill();
+    }
   };
 
   canvas.addEventListener('mousedown', (event) => {
@@ -30,14 +60,13 @@ const main = () => {
     const currentX = event.offsetX;
     const currentY = event.offsetY;
 
-    drawBox(startX, startY, currentX, currentY);
+    draw(startX, startY, currentX, currentY);
   });
 
   canvas.addEventListener('mouseup', () => {
     isDrawing = false;
   });
 
-  // upload image
   fileInput.addEventListener('change', () => {
     const file = fileInput.files[0];
     const reader = new FileReader();
@@ -45,26 +74,27 @@ const main = () => {
     reader.onload = (event) => {
       const image = new Image();
       image.onload = () => {
+        setCanvasDimensions();
         drawImageOnCanvas(image);
       };
       image.src = event.target.result;
     };
-    // read image
+
     reader.readAsDataURL(file);
   });
 
-  const drawBox = (startX, startY, endX, endY) => {
-    const width = endX - startX;
-    const height = endY - startY;
-    const selectedColor = colorPicker.value;
+  pencilButton.addEventListener('click', () => {
+    setTool('pencil');
+  });
 
-    context.strokeStyle = 'black';
-    context.lineWidth = 2;
-    context.fillStyle = selectedColor;
-    context.beginPath();
-    context.rect(startX, startY, width, height);
-    context.fill();
-  };
+  eraserButton.addEventListener('click', () => {
+    setTool('eraser');
+  });
+
+  boxButton.addEventListener('click', () => {
+    setTool('box');
+  });
+
   setCanvasDimensions();
 };
 
